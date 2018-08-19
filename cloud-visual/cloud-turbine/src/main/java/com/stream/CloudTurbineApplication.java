9b9211f2bdfc9b795d5eb87aaa92be7ceb8dffb1
Stream <streamplume@gmail.com>
2018-08-19 15:07:53 +0800

    提交修改内容

--- cloud-visual/cloud-turbine/src/main/java/com/stream/CloudTurbineApplication.java
+++ cloud-visual/cloud-turbine/src/main/java/com/stream/CloudTurbineApplication.java
@@ -2,9 +2,11 @@ package com.stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableTurbine
 public class CloudTurbineApplication {
 
